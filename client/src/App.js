4bf30ff0d7d2830d74a8a7822383c0ec673fb24e
Zigy <zigybass@gmail.com>
2019-12-05 17:14:06 -0500

    added server and React boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render () {
+  return (
+    <div className="App">
+      <h3>Visuals</h3>
+    </div>
+  );
+  }
+}
+
+export default App;
